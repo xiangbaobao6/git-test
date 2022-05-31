d2de21195352d7ad0cb0d6f91511ea5d92e4a3ef
xiangjialiang <1272513833@qq.com>
2022-05-31 17:24:01 +0800

    你好修改

--- src/main/java/com/njwbhz/App.java
+++ src/main/java/com/njwbhz/App.java
@@ -10,6 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "hehe world" );
-        System.out.println( "haha world" );
+        System.out.println( "你好，git" );
     }
 }
